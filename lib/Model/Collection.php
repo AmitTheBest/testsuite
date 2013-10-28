8731bf2377ece79967884556854412ade64a0aeb
Romans Malinovskis <r@agiletech.ie>
2013-10-28 18:34:40 +0000

    add basic models

---
+++ lib/Model/Collection.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * This model reresents a collection of test-sets. Collection 
+ * is usually contributed by a single developer. Typically add-on would
+ * come with a single collection of test-sets. As you iterate through this
+ * model, it will automatically locate all the test-collections of all
+ * the installed add-ons as well as your application.
+ */
+namespace testsuite;
+class Model_Set extends Model {
+
+    function init() {
+    }
+
+}
